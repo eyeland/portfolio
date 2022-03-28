b140686b23b36657ec4027ec2f42ec93134e5d7f
eyeland <63192606+eyeland@users.noreply.github.com>
2022-03-28 04:42:13 -0400

    add about me screen

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ const aboutMeSelector = document.querySelector('#aboutMe');
 const headerBar = document.getElementById('headerBar');
 const footerIcons = document.getElementById('footerIcons');
 const consoleText = document.getElementById('consoleText');
+const navBar = document.getElementById('navBar');
+const aboutMeAfter = document.getElementById('aboutMeAfter');
 
 
-const fadeScreenOut = () => {
+
+
+// Click on Bryant Code //
+
+
+const fadeOnBryant = () => {
     aboutMe.style.animation="aboutMeFade 2s ease-in";
     contact.style.animation="aboutMeFade 2s linear";
     portfolio.style.animation="aboutMeFade 2s ease-out";
@@ -19,6 +26,7 @@ const fadeScreenOut = () => {
     footerIcons.style.animation="downFade 2s ease-in";
     consoleText.style.animation="downFade 2s ease-out";
     setTimeout(() =>{
+
         aboutMe.style.right = "180%";
         contact.style.right = "180%";
         portfolio.style.right = "180%";
@@ -26,25 +34,67 @@ const fadeScreenOut = () => {
 
         footerIcons.style.bottom = "-65%";
         consoleText.style.bottom = "-65%";
+        bryant.style.maxHeight = "60%";
+        bryant.style.maxWidth = "60%";
+        bryant.style.left = "15%";
         
-    }, 1700)
-
-
+    }, 1700);
 }
 
 
+// Click fade on bryant //
 
+
+//Begin of Click fade on About Me //
+
+const fadeOnAboutMe = () => {
+  
+    contact.style.animation="aboutMeFade 2s linear";
+    portfolio.style.animation="aboutMeFade 2s ease-out";
+    headerBar.style.animation="aboutMeFade 2s ease-out";
+    // Down Animation //
+    footerIcons.style.animation="downFade 2s ease-in";
+    consoleText.style.animation="downFade 2s ease-out";
+    //Left Animation //
+    bryant.style.animation = "leftFade 2s linear";
+    navBar.style.animation = "aboutMeClick 1.7s ease-in";
+    aboutMeAfter.style.animation = "aboutMeBox 1.7s ease-in";
+
+    
+   
+
+    // position: absolute;
+    // bottom: 79%;
+    // right: 25%;
+
+
+    setTimeout(() =>{
+        // aboutMe.style.right = "180%";
+        aboutMeAfter.style.top = "25%";
+        contact.style.right = "180%";
+        portfolio.style.right = "180%";
+        headerBar.style.right = "180%";
+        footerIcons.style.bottom = "-65%";
+        consoleText.style.bottom = "-65%";
+        bryant.style.left = "120%";  
+        navBar.style.top = "0%";
+        
+    }, 1700);
+}
+
+
+// End of Click fade on About Me //
 
 
 
 aboutMe.addEventListener('click', () => {
-   
+   fadeOnAboutMe();
 })
 
 
 
 bryant.addEventListener('click', () => {
-    fadeScreenOut();
+    fadeOnBryant();
 })
 
 
