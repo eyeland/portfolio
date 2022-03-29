bafd5f5b6e4e5c54ad36dbc01de4dd57a3bd5100
eyeland <63192606+eyeland@users.noreply.github.com>
2022-03-29 16:04:04 -0400

    add bug fixes for reverse animation

--- script.js
+++ script.js
@@ -17,6 +17,58 @@ const xButton = document.getElementById('xButton');
 
 
 
+const removeAllAnimations = () => {
+
+    setTimeout(() => {
+        aboutMe.style.removeProperty('animation');
+        contact.style.removeProperty('animation');
+        portfolio.style.removeProperty('animation');
+        headerBar.style.removeProperty('animation');
+        footerIcons.style.removeProperty('animation');
+        consoleText.style.removeProperty('animation');
+        bryant.style.removeProperty('animation');
+        portfolioPage.style.removeProperty('animation');
+        contactInfoScreen.style.removeProperty('animation');
+        
+
+    }, 2000)
+
+}
+
+const reverseAnimations = () => {
+    aboutMe.style.animation="aboutMeFade 2s 0s 1 reverse";
+    contact.style.animation="aboutMeFade 2s 0s 1 reverse";
+    portfolio.style.animation="aboutMeFade 2s 0s 1 reverse";
+    headerBar.style.animation="headerBarFade 2s 0s 1 reverse";
+    // Down Animation //
+    footerIcons.style.animation="downFade 2s 0s 1 reverse";
+    consoleText.style.animation="downFade 2s 0s 1 reverse";
+    //Left Animation //
+    bryant.style.animation = "leftFade 2s 0s 1 reverse";
+    navBar.style.animation = "aboutMeClick 1.7s 0s 1 reverse";
+    aboutMeAfter.style.animation = "aboutMeBox 1.7s 0s 1 reverse";
+    portfolioPage.style.animation = "portfolio 2s 0s 1 reverse";
+    contactInfoScreen.style.animation = "contactClick 1.7s 0s 1 reverse";
+
+
+    setTimeout(() =>{
+        aboutMe.style.right = "0%";
+        aboutMeAfter.style.top = "120%";
+        contact.style.right = "0%";
+        portfolio.style.right = "0%";
+        headerBar.style.left = "0%";
+        footerIcons.style.bottom = "0%";
+        consoleText.style.bottom = "-0%";
+        bryant.style.left = "72%";  
+        navBar.style.top = "25%";
+        portfolioPage.style.top = "120%";
+        contactInfoScreen.style.left = "120%";
+        
+    }, 1700);
+
+}
+
+
 
 // Click on Bryant Code //
 
@@ -25,7 +77,7 @@ const fadeOnBryant = () => {
     aboutMe.style.animation="aboutMeFade 2s ease-in";
     contact.style.animation="aboutMeFade 2s linear";
     portfolio.style.animation="aboutMeFade 2s ease-out";
-    headerBar.style.animation="aboutMeFade 2s ease-out";
+    headerBar.style.animation="headerBarFade 2s ease-out";
     // Down Animation //
     footerIcons.style.animation="downFade 2s ease-in";
     consoleText.style.animation="downFade 2s ease-out";
@@ -34,7 +86,7 @@ const fadeOnBryant = () => {
         aboutMe.style.right = "180%";
         contact.style.right = "180%";
         portfolio.style.right = "180%";
-        headerBar.style.right = "180%";
+        headerBar.style.left = "0%";
 
         footerIcons.style.bottom = "-65%";
         consoleText.style.bottom = "-65%";
@@ -43,6 +95,7 @@ const fadeOnBryant = () => {
         bryant.style.left = "15%";
         
     }, 1700);
+    removeAllAnimations();
 }
 
 
@@ -55,7 +108,7 @@ const fadeOnAboutMe = () => {
   
     contact.style.animation="aboutMeFade 2s linear";
     portfolio.style.animation="aboutMeFade 2s ease-out";
-    headerBar.style.animation="aboutMeFade 2s ease-out";
+    headerBar.style.animation="headerBarFade 2s ease-out";
     // Down Animation //
     footerIcons.style.animation="downFade 2s ease-in";
     consoleText.style.animation="downFade 2s ease-out";
@@ -69,13 +122,14 @@ const fadeOnAboutMe = () => {
         aboutMeAfter.style.top = "25%";
         contact.style.right = "180%";
         portfolio.style.right = "180%";
-        headerBar.style.right = "180%";
+        headerBar.style.left = "0%";
         footerIcons.style.bottom = "-65%";
         consoleText.style.bottom = "-65%";
         bryant.style.left = "120%";  
         navBar.style.top = "0%";
         
     }, 1700);
+    removeAllAnimations();
 }
 // End of Click fade on About Me //
 
@@ -86,7 +140,7 @@ const fadeOnContact = () => {
   
     aboutMe.style.animation="aboutMeFade 1.7s ease-in";
     portfolio.style.animation="aboutMeFade 2s ease-out";
-    headerBar.style.animation="aboutMeFade 2s ease-out";
+    headerBar.style.animation="headerBarFade 2s ease-out";
     // Down Animation //
     footerIcons.style.animation="downFade 2s ease-in";
     consoleText.style.animation="downFade 2s ease-out";
@@ -101,7 +155,7 @@ const fadeOnContact = () => {
         contactInfoScreen.style.left = "0%";
         aboutMe.style.right = "180%";
         portfolio.style.right = "180%";
-        headerBar.style.right = "180%";
+        headerBar.style.left = "0%";
         footerIcons.style.bottom = "-65%";
         consoleText.style.bottom = "-65%";
         bryant.style.left = "120%";  
@@ -123,7 +177,7 @@ const fadeOnPortfolio = () => {
     contact.style.animation="aboutMeFade 2s linear";
     aboutMe.style.animation="aboutMeFade 1.7s ease-in";
     portfolio.style.animation="aboutMeFade 2s ease-out";
-    headerBar.style.animation="aboutMeFade 2s ease-out";
+    headerBar.style.animation="headerBarFade 2s ease-out";
     // Down Animation //
     footerIcons.style.animation="downFade 2s ease-in";
     consoleText.style.animation="downFade 2s ease-out";
@@ -137,35 +191,26 @@ const fadeOnPortfolio = () => {
         contact.style.right = "180%";
         aboutMe.style.right = "180%";
         portfolio.style.right = "180%";
-        headerBar.style.right = "180%";
+        headerBar.style.left = "-120%";
         footerIcons.style.bottom = "-65%";
         consoleText.style.bottom = "-65%";
         bryant.style.left = "120%";  
-        navBar.style.top = "0%";
+        navBar.style.top = "120%";
         portfolioPage.style.top = "10%";
-        portfolioPage.style.removeProperty('animation');
+        
+
         
     }, 1700);
+
+    removeAllAnimations();
+    
    
 }
 
 
 const backToHome = () =>{
-    portfolioPage.style.animation="portfolio 1.7s 0s 1 reverse";
-    
-    
-    setTimeout(() =>{
-        contact.style.right = "180%";
-        aboutMe.style.right = "180%";
-        portfolio.style.right = "180%";
-        headerBar.style.right = "180%";
-        footerIcons.style.bottom = "-65%";
-        consoleText.style.bottom = "-65%";
-        bryant.style.left = "120%";  
-        navBar.style.top = "0%";
-        portfolioPage.style.top = "10%";
-        
-    }, 1700);
+    reverseAnimations();
+    removeAllAnimations();
 
 }
 
@@ -184,6 +229,7 @@ bryant.addEventListener('click', () => {
 
 contact.addEventListener('click', () => {
     fadeOnContact();
+    removeAllAnimations();
 })
 
 portfolio.addEventListener('click', () => {
@@ -192,6 +238,5 @@ portfolio.addEventListener('click', () => {
 
 xButton.addEventListener('click', () => {
     backToHome();
-    console.log('button is working')
 })
 
